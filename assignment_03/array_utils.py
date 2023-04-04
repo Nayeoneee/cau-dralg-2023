34f605f8986f669376001748c262d2945014562b
Nayeoneee <129523145+Nayeoneee@users.noreply.github.com>
2023-04-04 22:22:29 +0900

    Update array_utils.py

--- assignment_03/array_utils.py
+++ assignment_03/array_utils.py
@@ -3,28 +3,48 @@ import os.path as osp
 
 # 문제(1)
 def get_max(arr):
-    # 코드 작성...
-    return 0
+    if len(arr) == 0: 
+        return None 
+    max_val = arr[0] 
+    for i in range(1, len(arr)): 
+        if arr[i] > max_val: 
+            max_val = arr[i]
+    return max_val if max_val is not None else 0
 
 
 def get_min(arr):
-    # 코드 작성...
-    return 0
+    if len(arr) == 0: 
+        return None 
+    min_val = arr[0] 
+    for i in range(1, len(arr)): 
+        if arr[i] < min_val:
+            min_val = arr[i]
+    return min_val if min_val is not None else 0
 
 
 def get_sum(arr):
-    # 코드 작성...
-    return 0
+    s = 0
+    for i in range(len(arr)):
+        s += arr[i]
+    return s if len(arr) > 0 else 0
 
 
 def get_mean(arr):
-    # 코드 작성...
-    return 0
+     = len(arr)
+    if n == 0:
+        return 0
+    s = 0
+    for i in range(n):
+        s += arr[i]
+    return s / n
 
 
 def get_median(arr):
-    # 코드 작성...
-    return 0
+    n = len(arr)
+    if n % 2 == 0:
+        return (arr[n//2 - 1] + arr[n//2]) / 2
+    else:
+        return arr[n//2]
 
 
 # 문제(2)
@@ -33,7 +53,12 @@ def count_files(paths):
     paths: 디렉토리 및 파일 경로들이 포함되어 있는 list 객체.
     """
     n_files = 0
-    # 코드 작성...
+    fpaths = set(filter(lambda p: osp.splitext(p)[1] != '', paths))
+    for path in paths:
+        if path not in fpaths:
+            dirname = osp.dirname(path)
+            if dirname in fpaths:
+                n_files += 1
     return n_files
 
 
@@ -43,7 +68,10 @@ def filter_fname(fpaths, word):
     word: 파일 이름에 포함되어야 하는 단어.
     """
     fpaths_filtered = []
-    # 코드 작성...
+    for path in fpaths:
+        fname = osp.basename(path)
+        if word in fname:
+            fpaths_filtered.append(path)
     return fpaths_filtered
 
 
@@ -53,7 +81,10 @@ def filter_ext(fpaths, ext):
     ext: 파일확장자 (예: "jpg", "png")
     """
     fpaths_filtered = []
-    # 코드 작성...
+    for fpath in fpaths:
+        basename = osp.basename(fpath)
+        if '.' in basename and basename.split('.')[-1] == ext:
+            fpaths_filtered.append(fpath)
     return fpaths_filtered
 
 
@@ -64,7 +95,14 @@ def change_fname(fpaths, old_word, new_word):
     new_word: 파일 이름에 새롭게 추가할 단어.
     """
     fpaths_new = []
-    # 코드 작성...
+    for fpath in fpaths:
+        fname = osp.basename(fpath)
+        if old_word in fname:
+            fname_new = fname.replace(old_word, new_word)
+            fpath_new = osp.join(osp.dirname(fpath), fname_new)
+            fpaths_new.append(fpath_new)
+        else:
+            fpaths_new.append(fpath)
     return fpaths_new
 
 
@@ -74,5 +112,8 @@ def change_dpath(fpaths, dpath):
     dpath: 새로운 디렉토리 경로.    
     """
     fpaths_new = []
-
+    for fpath in fpaths:
+        fname = osp.basename(fpath)
+        fpath_new = osp.join(dpath, fname)
+        fpaths_new.append(fpath_new)
     return fpaths_new
